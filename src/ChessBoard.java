87061bc204f198673ee25fa22c144ba6ef18bfd7
Cyrill <cyrillkuettel@gmail.com>
2021-07-05 21:56:10 +0200

    painting ChessBoard

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -1,8 +1,7 @@
 import javax.swing.JFrame;
 
 
-import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.*;
 import javax.swing.JPanel;
 
 public class ChessBoard extends JPanel{
@@ -14,9 +13,12 @@ private final int BOARD_LEN;
     }
 
 
-    public void paint(Graphics gp)
-    {
+    public void paint(Graphics gp) {
+        Color saddlebrown = new Color(139,69,19);
+        Color chocolateBrown = new Color(210,105,30);
 
+
+        gp.setColor(saddlebrown);
         gp.fillRect(50, 50, 600, 600);
         for(int x=50; x<=600; x+=150)
         {
@@ -27,15 +29,22 @@ private final int BOARD_LEN;
         }
 
 
-        for(int x=125; x<=600; x+=150)
-        {
+        for(int x=125; x<=600; x+=150) {
             for(int y=125; y<=600; y+=150)
             {
                 gp.clearRect(x, y, 75, 75);
             }
         }
-        gp.drawRect(50,50,600, 600);
+        // gp.setColor(Color.BLACK);
+       // gp.drawRect(50,50,600, 600); // square around the board
+
+        Graphics2D g2d = (Graphics2D) gp.create();
+        g2d.setStroke(new BasicStroke(4, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+        g2d.setColor(Color.BLACK);
+        g2d.drawLine(0,0, 20, 35);
     }
+
+
 
 
     public static void main(String[] args) {
