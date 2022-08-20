6c5103cb3a89eab93862320532c66d7c010efcea
Cyrill <cyrillkuettel@gmail.com>
2022-08-21 00:58:30 +0200

    refactoring

---
+++ test/Genetic_Algorithm/CheckSolutionFoundTest.java
@@ -0,0 +1,13 @@
+package Genetic_Algorithm;
+
+/**
+ * We need a function to determine that an Individual has reached a state of completeness.
+ * Optimal state in a sense that a valid tour has been found.
+ */
+public class CheckSolutionFoundTest {
+
+    void checkSolutionFound() {
+
+    }
+
+}
